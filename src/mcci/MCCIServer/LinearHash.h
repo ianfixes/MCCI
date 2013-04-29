23175e1565ff17ecb12a9fa79b9a9a39200ba9d0
Ian Katz <ifreecarve@gmail.com>
2013-04-28 22:19:24 -0400

    linear hash table implementation

---
+++ src/mcci/MCCIServer/LinearHash.h
@@ -0,0 +1,105 @@
+
+#pragma once
+
+#include <map>
+
+using namespace std;
+
+/**
+   A simple hash table that uses f(i) = i for the hash function (so does Sun's hash table implementation)
+
+   This hash table is designed for speed and for integer keys (key should be some variant of int/long/etc)
+
+   It is assumed that contiguous blocks of integer keys will be hashed.
+   
+ */
+template <typename Key, typename Data> class LinearHash
+{
+
+  protected:
+    map <Key, Data>** m_container;
+    unsigned int m_size;
+    
+  public:
+
+    // to deal with collisions, we use a map (implemented as a tree usually). initialize an array of trees
+    LinearHash(unsigned int size)
+    {
+        this->m_size = size;
+        this->m_container = new map<Key, Data>*[this->m_size];
+
+        for (int i = 0; i < this->m_size; ++i)
+            this->m_container[i] = new map<Key, Data>();
+    };
+
+    
+    ~LinearHash()
+    {
+        for (int i = 0; i < this->m_size; ++i)
+            delete this->m_container[i];
+        delete[] this->m_container; // TODO: check proper delete syntax
+    };
+
+
+    // return the size of the hash table
+    void get_size() const
+    {
+        return this->m_size;
+    }
+
+    // return the number of elements in the hash table
+    unsigned int count() const
+    {
+        unsigned int sum = 0;
+
+        for (unsigned int i = 0; i < this->m_size; ++i)
+            sum += this->m_container[i]->size();
+
+        return sum;
+    }
+
+    
+    // get the maximum key collisions on any given bucket
+    unsigned int max_collisions() const
+    {
+        unsigned int max = 0;
+
+        for (unsigned int i = 0; i < this->m_size; ++i)
+            if (max < this->m_container[i]->size())
+                max = this->m_container[i]->size();
+
+        return max;
+    }
+
+    // explicitly insert an element into the hash
+    void insert(Key k, Data d)
+    {
+        (*(this->m_container[k % this->m_size]))[k] = d;
+    }
+
+    // explicitly remove a key from the hash
+    void remove(Key k)
+    {
+        this->m_container[k % this->m_size]->erase(k);
+    }
+
+    // check existence of a hashed value
+    bool has_key(Key k)
+    {
+        unsigned int idx = k % this->m_size;
+        return this->m_container[idx]->end() != this->m_container[idx]->find(k);
+    }
+
+    // array-style access to the hash
+    Data& operator[] (Key k)
+    {
+        return (*(this->m_container[k % this->m_size]))[k];
+    }
+
+    void clear()
+    {
+        for (int i = 0; i < this->m_size; ++i)
+            this->m_container[i]->clear();
+    }
+    
+};
---
+++ src/mcci/MCCIServer/LinearHashTest.cpp
@@ -0,0 +1,50 @@
+
+#include "LinearHash.h"
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    LinearHash<unsigned long, string> lh(101);
+
+    lh.insert(3000, "three thousand");
+    lh.insert(1000000, "one million");
+    lh[37] = "thirty seven";
+
+    printf("\nExpect 3 elements in hash, count() = %d", lh.count());
+
+    printf("\n3001 in hash? %d", lh.has_key(3001));
+    printf("\n3000 in hash? %d", lh.has_key(3000));
+    printf("\nValue of 3000 is %s", lh[3000].c_str());
+
+    lh.remove(3000);
+    printf("\nRemoved 3000.  3000 in hash? %d", lh.has_key(3000));
+
+    printf("\n");
+
+    printf("\n37 = %s", lh[37].c_str());
+    lh[37] = "I'm not old";
+    printf("\n37 = %s", lh[37].c_str());
+
+    printf("\nExpect 2 elements in hash, count() = %d", lh.count());
+    
+    // step sizes 1, 11, 21 ... 101
+    // demonstrating that we only get collisions when step size = hash table size
+    for (int step = 0; step < 12; ++step)
+    {
+        printf("\n\nClearing, to insert 303 elements, step size = %d", (step * 10 + 1));
+        lh.clear();
+        
+        for (int i = 0; i < 303; ++i)
+            lh.insert((step * 10 + 1) * i, "hello");
+        
+        printf("\nCount is now %d, max_collisions is %d", lh.count(), lh.max_collisions());
+    }
+
+
+    printf("\n\n");
+    
+    return 0;
+}
